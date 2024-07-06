342685973e23f29e6f447a5653724b01c9a18129
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2024-07-07 02:25:29 +0900

    [Silver III] Title: 햄버거 분배, Time: 112 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -8,18 +8,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long N = Integer.parseInt(st.nextToken()); //식탁의 길이
-        long K = Integer.parseInt(st.nextToken()); //햄버거 선택 가능 거리
-        long count = 0;
+        int N = Integer.parseInt(st.nextToken()); //식탁의 길이
+        int K = Integer.parseInt(st.nextToken()); //햄버거 선택 가능 거리
+        int count = 0;
         char table[] = br.readLine().toCharArray();
 
-        for(long i = 0; i < N; i++) {
-            if(table[(int) i] == 'P') {
-                long start = (i-K>=0)?i-K:0;
-                long end = (i+K<N)?i+K:N-1;
-                for(long j = start; j <= end; j ++) {
-                    if (table[(int) j] == 'H') {
-                        table[(int) j] = 'N';
+        for(int i = 0; i < N; i++) { //테이블 순회
+            if(table[i] == 'P') {
+                int start = Math.max(i - K, 0);
+                int end = Math.min(i + K, N-1);
+                for(int j = start; j <= end; j ++) {
+                    if (table[j] == 'H') {
+                        table[j] = 'N';
                         count++;
                         break;
                     }
@@ -27,6 +27,5 @@ public class Main {
             }
         }
         System.out.println(count);
-
     }
 }
