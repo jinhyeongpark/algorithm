cbe21f24a9cce72746e685edc08032d4806e2a09
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2024-09-12 19:23:27 +0900

    [Bronze I] Title: 평균, Time: 264 ms, Memory: 19068 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,20 +1,20 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-
-        long sum = 0;
-        long max = 0;
-
-        for (int i = 0; i < N; i++) {
-            int temp = sc.nextInt();
-            if (temp > max) max = temp;
-            sum += temp;
+        int scores[] = new int[N];
+        for(int i = 0; i < N; i++) {
+            scores[i] = sc.nextInt();
         }
-        System.out.println(sum*100.0/max/N);
-        sc.close();
+        Arrays.sort(scores);
+        double maxScore = scores[N-1];
+        double sum = 0;
+        for(int score : scores) {
+            sum += score/maxScore*100;
+        }
+        System.out.printf("%.2f", sum/N);
+        }
+
     }
-}
