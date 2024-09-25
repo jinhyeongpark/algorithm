b331d49b078f34f92696ce8a2229c3b58b8a39a3
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2024-09-25 13:59:35 +0900

    [Silver II] Title: 스택 수열, Time: 304 ms, Memory: 27716 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -5,42 +5,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine()); // 입력될 숫자의 수
-        int[] arr = new int[n]; // n개의 숫자를 저장할 배열
+        Stack<Integer> stack = new Stack<>(); //스택
+        int n = Integer.parseInt(br.readLine()); //입력 받을 숫자의 수
+        int add = 1;
+        boolean flag = true;
 
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
+            int num = Integer.parseInt(br.readLine());
 
-        Stack<Integer> numStack = new Stack<>(); // 숫자를 쌓을 스택
-        List<String> res = new ArrayList<>(); // +와 -를 저장할 리스트
-        int add = 1; // 1부터 시작
-        boolean flag = true; // 성공 여부 체크
-
-        for (int num : arr) { // arr 리스트에서 숫자를 하나씩 가져옴
             while (add <= num) {
-                numStack.push(add++);
-                res.add("+");
+                stack.push(add++);
+                sb.append("+\n");
             }
 
-            if (!numStack.isEmpty() && num == numStack.peek()) {
-                numStack.pop();
-                res.add("-");
+            if (stack.peek() == num) {
+                stack.pop();
+                sb.append("-\n");
             } else {
                 flag = false;
                 break;
             }
         }
 
-        if (flag) {
-            for (String op : res) {
-                sb.append(op).append("\n");
-            }
-        } else {
-            sb.append("NO\n");
-        }
-
-        System.out.print(sb.toString());
+        System.out.print(flag ? sb.toString() : "NO\n");
     }
 }
