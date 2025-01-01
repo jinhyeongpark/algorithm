8cc346b63d66ad89603318dfac77957f3f4e2474
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-01-01 16:01:24 +0900

    [Gold IV] Title: 부분합, Time: 260 ms, Memory: 23948 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken()); // 수열 길이
+        int M = Integer.parseInt(st.nextToken()); // 기준 값
+
+        int[] nums = new int[N + 1];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int res = N + 1;
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+
+        while (start <= end && end <= N) { // 조건 수정
+            if (sum >= M) { // 조건 순서 변경
+                res = Math.min(res, end - start);
+                sum -= nums[start++];
+            } else if (end < N) {
+                sum += nums[end++];
+            } else { // sum < M && end == N 인 경우
+                break;
+            }
+        }
+
+        System.out.println(res == N + 1 ? 0 : res);
+    }
+}
