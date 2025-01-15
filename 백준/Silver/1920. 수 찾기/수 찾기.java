023b085a5078b581b2d38301e33d4b79dcf19052
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-01-15 15:17:05 +0900

    [Silver IV] Title: 수 찾기, Time: 628 ms, Memory: 42092 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,35 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
+    static BufferedWriter bw;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int N = Integer.parseInt(br.readLine());
         int[] A = new int[N];
-
-        StringTokenizer nums = new StringTokenizer(br.readLine(), " ");
-
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(nums.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(A);
 
         int M = Integer.parseInt(br.readLine());
-
-        nums = new StringTokenizer(br.readLine(), " ");
-
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < M; i++) {
-            int target = Integer.parseInt(nums.nextToken());
-            if (Arrays.binarySearch(A, target) >= 0) {
-                System.out.println(1);
+            if (binarySearch(A, 0, A.length-1, Integer.parseInt(st.nextToken()))) {
+                bw.write(1 + "\n");
             } else {
-                System.out.println(0);
+                bw.write(0 + "\n");
             }
-            ;
+
         }
+        bw.flush();
+        bw.close();
+    }
+    private static boolean binarySearch(int[] arr, int start, int end, int num) throws IOException {
+        if (start > end) return  false;
+        int mid = (start + end) / 2;
+        if (num == arr[mid]) {
+            return true;
+        }
+        if (num < arr[mid]) return binarySearch(arr, start, mid-1, num);
+        else return binarySearch(arr, mid+1, end, num);
     }
 }
