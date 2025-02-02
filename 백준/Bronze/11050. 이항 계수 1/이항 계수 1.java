bd410541951acd4bcb443ef38115509f242eb643
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-02-03 00:35:18 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -4,23 +4,27 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    static int[][] nr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] nums = br.readLine().split(" ");
         int n = Integer.parseInt(nums[0]);
         int r = Integer.parseInt(nums[1]);
-        System.out.println(combination(n, r));
-    }
+        nr = new int[n+1][n+1];
 
-    static int combination(int n, int r) {
-        return fact(n) / (fact(n-r) * fact(r));
-    }
-
-    static int fact(int num) {
-        int result = 1;
-        for (int i = num; i >= 1; i--) {
-            result *= i;
+        for (int i = 0; i <= n; i++) {
+            nr[i][1] = i;
+            nr[i][0] = 1;
+            nr[i][i] = 1;
         }
-        return result;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                nr[i][j] = nr[i-1][j] + nr[i-1][j-1];
+            }
+        }
+
+        System.out.println(nr[n][r]);
     }
 }
