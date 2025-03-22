95985cdfadb95506082e9ced680d2354fe4c9fa6
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-03-23 01:36:05 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 440 ms, Memory: 91516 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -27,16 +27,15 @@ public class Main {
             if (count > K) { //홀수 카운트가 K보다 많은 경우
                 if (arr[left] % 2 == 1) count--;
                 left++; //우로 이동
-                continue; 
                 //확장이 아닌 경우는 result를 갱신하지 않는다
                 //확장 대상이 홀수이면 count를 소비하지 않고 늘릴 수 있기 때문이다
             } else { //count <= K
                 if(arr[right] % 2 == 1) count++; //홀수의 경우 카운트를 올리고
                 right++; //우로 이동
+                //right - left는 현재 윈도우의 길이
+                //count를 빼면 실제로 남는 짝수 수를 알 수 있다
+                result = Math.max(result, right-left-count);
             }
-            //right - left는 현재 윈도우의 길이
-            //count를 빼면 실제로 남는 짝수 수를 알 수 있다
-            result = Math.max(result, right-left-count);
         }
         System.out.println(result);
     }
