0a8e0026e7532674d68742b7f9076d4e86f0742a
박진형 <156793718+jinhyeongpark@users.noreply.github.com>
2026-03-09 17:12:07 +0900

    [Silver I] Title: 후보 추천하기, Time: 124 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
@@ -8,74 +8,52 @@ import java.util.StringTokenizer;
 public class Main {
 
     static class Student {
-        int id;
-        int recommended;
-        int order;
+        int id, recommended, order;
 
         Student(int id, int order) {
             this.id = id;
-            this.recommended = 0;
+            this.recommended = 1; // 등록과 동시에 추천 1회
             this.order = order;
         }
-        void recommend() {
-            this.recommended++;
-        }
     }
-
-    static ArrayList<Student> students;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        students = new ArrayList<>();
-
-        int numOfRecommendation = Integer.parseInt(br.readLine());
+        int totalRec = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < numOfRecommendation; i++) {
-            int studentId = Integer.parseInt(st.nextToken());
-            // 이미 등록된 학생이면 추천 수 증가
-            if (isRegistered(studentId)) {
-                getStudent(studentId).recommend();
-            } else { // 그렇지 않으면 틀의 수가 초과되었는지 확인
-                if (students.size() < N) { // 초과되지 않았다면 등록 후 추천 수 증가
-                    Student student = new Student(studentId,i);
-                    student.recommend();
-                    students.add(student);
-                } else { // 초과되었다면 가장 낮은 추천 수의 학생을 제거하고 새로 등록 후 추천 수 증가
-                    Collections.sort(students, (s1, s2) -> {
-                        if (s1.recommended == s2.recommended) return s1.order - s2.order;
-                        return s1.recommended - s2.recommended;
-                    });
-                    students.remove(0);
-                    Student newStudent = new Student(studentId, i);
-                    newStudent.recommend();
-                    students.add(newStudent);
+        ArrayList<Student> frames = new ArrayList<>();
+
+        for (int i = 0; i < totalRec; i++) {
+            int id = Integer.parseInt(st.nextToken());
+
+            Student target = null;
+            for (Student s: frames) {
+                if (s.id == id) {
+                    target = s;
+                    break;
                 }
             }
-        }
-        ArrayList<Integer> ids = new ArrayList<>();
-        for (Student student : students) {
-            ids.add(student.id);
-        }
-        Collections.sort(ids);
-        for (int id : ids) {
-            System.out.print(id + " ");
-        }
-    }
 
-    private static boolean isRegistered(int id) {
-        for (Student student : students) {
-            if (student.id == id) return true;
+            if (target != null) {
+                target.recommended++;
+            } else {
+                if (frames.size() == N) {
+                    Collections.sort(frames, (s1, s2) -> {
+                        if (s1.recommended == s2.recommended) {
+                            return s1.order - s2.order;
+                        } return s1.recommended - s2.recommended;
+                    });
+                    frames.remove(0);
+                }
+                frames.add(new Student(id, i));
+            }
         }
-        return false;
-    }
-
-    private static Student getStudent(int id) {
-        for (Student student : students) {
-            if (student.id == id) return student;
-        }
-        return null;
+        Collections.sort(frames, (s1, s2) -> s1.id - s2.id);
+        StringBuilder sb = new StringBuilder();
+        for (Student s : frames) sb.append(s.id).append(" ");
+        System.out.println(sb);
     }
 }
+
